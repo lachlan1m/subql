ca7fa2ec500d10ccf9e7e4fae7b23b0236bbdd3f
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2021-12-23 14:56:52 +1300

    Revert "Pipeline yarn stable version"

--- .github/actions/create-prerelease/remove-stable-version.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const myArgs = process.argv.slice(2);
-const pJson = require(`${myArgs[0]}/package.json`)
-
-if (pJson.stableVersion){
-    delete pJson.stableVersion
-    fs.writeFileSync(`${myArgs[0]}/package.json`, JSON.stringify(pJson, null, 2))
-}
